a1676192b72459568149d04a2f9d91c653e0ed80
James <jfoshee2@students.kennesaw.edu>
2016-11-22 21:11:53 -0500

    KSU-005: Attempted to redirect user
    User should be redirected to home page WIP

--- home/views.py
+++ home/views.py
@@ -1,7 +1,38 @@
 from django.http import HttpResponse
 from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.views.generic import View
+from .forms import UserForm
 
 
 def index(request):
     return render(request, "home/index.html")
+
+
+class UserFormView(View):
+    form_class = UserForm
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+            user = form.save(commit=False)
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user.set_password(password)
+            user.save()
+
+            # returns User objects if credentials are correct
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+
+                if user.is_active:
+                    login(request, user)
+                    return redirect('/home')
+
+
+
+
+
